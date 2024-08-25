1e8d19e6621769c3075e518f39279dcfae466771
Nicolas Mema <nicolas.a.mema@gmail.com>
2024-08-25 15:02:41 -0300

    #3 add VPC resources

---
+++ prod/vpc/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source = "git::https://github.com/nmema/terraform-baby-modules.git//services/vpc?ref=v0.0.2"
+
+  aws_region = "us-west-2"
+}
+
+terraform {
+  backend "s3" {
+    key            = "vpc/terraform.tfstate"
+    bucket         = "terraform-baby-state-prod"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-baby-locking"
+    encrypt        = true
+  }
+}
