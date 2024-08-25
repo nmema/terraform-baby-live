30f23956f54a23d538dbb34f015def2a754ffba4
Nicolas Mema <nicolas.a.mema@gmail.com>
2024-08-24 22:12:07 -0300

    #1 add S3 Backend

---
+++ stage/s3_state_backend/main.tf
@@ -0,0 +1,18 @@
+module "s3_backend" {
+  source = "git::https://github.com/nmema/terraform-baby-modules.git//services/s3_backend?ref=v0.0.1"
+
+  aws_region  = "us-west-2"
+  bucket_name = "terraform-baby-state-dev"
+  table_name  = "terraform-baby-locking"
+}
+
+# Comment this line if S3 backend is not created.
+terraform {
+  backend "s3" {
+    key            = "s3_state_backend/terraform.tfstate"
+    bucket         = "terraform-baby-state-dev"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-baby-locking"
+    encrypt        = true
+  }
+}
